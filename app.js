c27b4a942bac526bfc977159eadb7c1207036aa1
Syedanaskhi51 <154814876+Syedanaskhi51@users.noreply.github.com>
2024-03-04 21:33:18 +0500

    html and js created file

---
+++ app.js
@@ -0,0 +1,22 @@
+alert('hello world')
+
+alert('Please! enter a valid password.')
+
+console.log('syed anas')
+
+var abcc = ('hello')
+
+alert('hello world \n hello world \n hello world')
+
+alert('Welcome to js Land...\n Happy Coding!')
+
+
+var  abc = " hello \n people"
+alert(abc)
+
+var one =( " 8712837 ")
+
+alert ( "Hello... I can run Js through my web browser's console  ")
+
+alert('I am 19 years old')
+alert('you have visited this site 15 times')
